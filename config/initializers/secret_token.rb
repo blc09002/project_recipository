72524db9606b7f74ecc6ba9ca794210e10ea7717
Ben Climan <benjamin.climan@uconn.edu>
2012-10-22 15:03:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectRecipository::Application.config.secret_token = '7dfac9f54f530e272d2721702b04cab245d2e1cc63545b18d18da57acc1437fa162465a38d9e7d7e689856d0e8c9d86aa408f83d058e9d1183aa3d9eab5e4dba'
