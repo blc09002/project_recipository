a09f6b9429b0f1bd3ea67ea719c2f08a1b365d3f
Ben Climan <benjamin.climan@uconn.edu>
2012-11-12 16:40:15 -0500

    All files updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
-class User
-  attr_accessor :name, :email
-
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
-
-  def formatted_email
-    "#{@name} <#{@email}>"
+class UsersController < ApplicationController
+  def new
   end
 end
