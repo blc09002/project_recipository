607324796795bd5c47e0379ccd37c8af247cec40
Ben Climan <benjamin.climan@uconn.edu>
2012-11-12 12:58:30 -0500

    All files updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+# Return the full website title on each page
+
+def full_title (page_title)
+	base_title = "Recipository"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title}" | #{page_title}"
+	end
+end
 end
