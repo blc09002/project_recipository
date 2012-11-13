48ecaa58e428f47656e82aac2aa5c2835f8c219c
Ben Climan <benjamin.climan@uconn.edu>
2012-11-13 15:24:13 -0500

    Updated everything ... again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,12 @@ module ApplicationHelper
 
 # Return the full website title on each page
 
-def full_title (page_title)
-	base_title = "Recipository"
-
-
-if page_title.empty?
-		base_title
-	else
-		"#{base_title}" | #{page_title}"
+	def full_title (page_title)
+		base_title = "Recipository"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title}" | "#{page_title}"
+		end
 	end
-end
 end
