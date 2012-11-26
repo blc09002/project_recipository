9b6bd7571110816e85983bc97f9efe1ba40d5422
Ben Climan <benjamin.climan@uconn.edu>
2012-11-26 16:48:35 -0500

    d

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,28 +1,34 @@
 module SessionsHelper
 
-	def sign_in(user)
+ def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
-  	end
+  end
 
-  	def signed_in?
+  def signed_in?
     !current_user.nil?
-  	end
+  end
 
-  	def current_user=(user)
+  def current_user=(user)
     @current_user = user
-  	end
+  end
 
-  	def current_user
+  def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
-	end
+  end
 
-    def sign_out
-    self.current_user = nil
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def sign_out
+    current_user = nil
     cookies.delete(:remember_token)
-  	end
-  
-
-
-
-end
+  end
